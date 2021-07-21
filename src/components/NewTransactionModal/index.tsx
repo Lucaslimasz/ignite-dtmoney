fbd48852bf80828e5cc6f02e2eecc73cf62a1cbb
Lucaslimasz <id.lucas@outlook.com.br>
2021-07-21 15:52:34 -0300

    feat: consuming the fake api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,8 @@ import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 
+import { api } from '../../config/api';
+
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
 interface NewTransactionModalProps {
@@ -24,7 +26,9 @@ function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({title, value, category, type})
+    const data = {title, value, category, type}
+
+    api.post('/transactions', data)
   }
 
   return (
